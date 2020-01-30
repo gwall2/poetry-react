dfaef3cf185f775f1504e64e41455e583d715412
gwall2 <gwall2@kent.edu>
2020-01-29 19:02:25 -0500

    initial push of poetry app front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
+import axios from 'axios';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      poems:[]
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/poems/').then((res) => {
+      console.log(res)
+      this.setState({poems:res.data})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          _.map(this.state.poems, (poem)=>{
+            return (
+              <div>
+                <h1>
+                  {poem.title}
+                </h1>
+                {poem.content}
+              </div>
+            )
+          })
+        } 
+      </div>
+    );  
+  }
+  
 }
 
 export default App;
